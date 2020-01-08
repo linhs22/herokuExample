bc9e90b659e6ccf163bedd83c203b6c68df165a7
Henry Lin <henrylin58@gmail.com>
2020-01-08 10:50:45 -0800

    added json

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ app.get('/', (req, res) => {
     res.send("welcome to my API Site!");
 })
 
+app.get('/food', (req, res) => {
+    res.json({
+        name: "Ramen",
+        bestFood: true
+    })
+})
 
 app.listen(PORT, function() {
     console.log('listening on port ' + PORT);
